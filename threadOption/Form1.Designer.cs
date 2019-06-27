3e21be205d0a3ba93b4b0617ea255221310a0a0a
jinzhuge <52116801+jinzhuge@users.noreply.github.com>
2019-06-27 08:49:01 +0800

    09:00

--- threadOption/Form1.Designer.cs
+++ threadOption/Form1.Designer.cs
@@ -28,14 +28,26 @@ namespace threadOption
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(349, 219);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(533, 292);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -43,6 +55,8 @@ namespace threadOption
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- threadOption/Form1.cs
+++ threadOption/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,6 +16,8 @@ namespace threadOption
 {
     public partial class Form1 : Form
     {
+        static AutoResetEvent autoEvent = new AutoResetEvent(false);
+        
         public Form1()
         {
 
@@ -30,7 +33,29 @@ namespace threadOption
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Thanks!");
+
+            Console.WriteLine("sdf");
         }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Thread A = new Thread(() => Run());
+            A.Name = "thread A";
+            A.Start();
+
+            Thread.Sleep(3000);
+            autoEvent.Set();
+            Console.ReadLine();
+        }
+
+        static void Run()
+        {
+            string name = Thread.CurrentThread.Name;
+            Console.WriteLine("{0}:run start", name);
+            autoEvent.WaitOne();
+
+            Console.WriteLine("{0}: dowork", name);
+        }
+
     }
 }
